36bde52ab7384324679f1ca2c5541042db80caba
willenicholls85-eng <willenicholls85@gmail.com>
2025-10-24 19:48:58 +1000

    Update app.py

--- app.py
+++ app.py
@@ -276,9 +276,5 @@ def logout():
     
     return jsonify({'success': True})
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
 
 
